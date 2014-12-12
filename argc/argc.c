31518e069dc2b575ef5fbaccc9e4dc433249831f
Marco Schmid <fakE00r@gmail.com>
2014-12-12 15:20:27 +0100

    added argv

--- argc/argc.c
+++ argc/argc.c
@@ -1,46 +1,55 @@
 /* @(#)argc.c
-   
+
    skeleton for printing statistics about
    character distribution of arguments from command line
-   
-   * argc is the number of arguments typed -- including the program name.
-   * argv is an array of strings holding each command line argument
-   -- including the program name in the first array element.
-   * statistik is an array indicating for each character its frequency
-   * print_stat is a procedure printing the number of characters per character
-*/
+
+ * argc is the number of arguments typed -- including the program name.
+ * argv is an array of strings holding each command line argument
+ -- including the program name in the first array element.
+ * statistik is an array indicating for each character its frequency
+ * print_stat is a procedure printing the number of characters per character
+ */
 
 #include <stdio.h>
+#include <string.h>
 
 unsigned int statistik[256];
 
-void print_stat(unsigned int* statistik){
-  int i;
-  for(i=0; i<256; i++){
-	if(statistik[i] != 0 ) {
-	  if( i>31 && i<127 )
-		printf("%03d = 0x%02X = '%c' : %u\n", i, i, i, statistik[i] ) ;
-	  else
-		printf ( "%02Xh     : %u\n" , i , statistik[i] ) ;
-			
-	}
-  }
+void print_stat(unsigned int* statistik)
+{
+    int i;
+    for(i=0; i<256; i++)
+    {
+        if(statistik[i] != 0 ) 
+        {
+            if( i>31 && i<127 )
+                printf("%03d = 0x%02X = '%c' : %u\n", i, i, i, statistik[i] ) ;
+            else
+                printf ( "%02Xh     : %u\n" , i , statistik[i] ) ;
+        }
+    }
 }
 
 
-int main(int argc, char **argv){
-  
+int main(int argc, char **argv)
+{
+    int i;
+    memset(statistik, 0, 256*sizeof(int));
 
-  // 
-  // your code here
-  //
+    for (i=0; i<argc; i++)
+    {
+        int j = 0; 
+        while (argv[i][j] != 0)
+        {
+            statistik[(int)argv[i][j]]++; 
+            j++;
+        }
+    }
 
-  
-  print_stat(statistik);
-  
-  return 0;
+    print_stat(statistik);
+    return 0;
 }
- 
+
 
 
 
